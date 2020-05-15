f26790cdc3f5c123bb4e030f13991f3bb9a9458e
Guilherme Diego <guilherme.albino.francisco@gmail.com>
2020-05-15 18:48:30 -0300

    Update: Site

--- build.js
+++ build.js
@@ -4,23 +4,36 @@ const fs = require('fs');
 const moment = require('moment');
 const _ = require('lodash');
 
+Handlebars.registerHelper('baseUrl', function (path) {
+    return (process.env.BASE_URL || '') + path;
+})
+
+Handlebars.registerHelper('assetUrl', function (path) {
+    return (process.env.BASE_URL || '') + '/assets' + path;
+})
 
 const hbsFile = fs.readFileSync('./index.hbs')
 const template = Handlebars.compile(hbsFile.toString());
 
 const initialDate = moment(data.govInitialDate);
-const people = data.people.map(({ out, name, owner }, idx) => {
+const people = data.people.map(({ out, name, owner, start }, idx) => {
     const outDate = moment(out);
-    const isLast = idx == data.people.length - 1;
 
     return {
         idx,
         name,
         owner,
-        liveDays: outDate.diff(initialDate, 'days'),
+        liveDays: outDate.diff(start || initialDate, 'days'),
         outDate: outDate.format('DD/MM/YYYY'),
-        isLast,
     }
-}).sort((a, b) => b.idx - a.idx);
+});
 
-fs.writeFileSync('./index.html', template({ people }));
+const mostTimeOnGov = _.maxBy(people, 'liveDays').idx
+const lessTimeOnGov = _.minBy(people, 'liveDays').idx
+
+people[mostTimeOnGov].mostDays = true;
+people[lessTimeOnGov].lessDays = true;
+
+fs.writeFileSync('./index.html', template({
+    people: people.sort((a, b) => b.idx - a.idx)
+}));
